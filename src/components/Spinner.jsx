6b1b4e5b4171f0560a71511ea111a2373bacbd4d
xandro2021 <xandrocastro2021@gmail.com>
2023-11-01 13:49:25 -0600

    Display result and style it

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,7 +2,7 @@ import "../styles/Spinner.css";
 
 const Spinner = () => {
   return (
-    <div className="mt-6 py-4">
+    <div className="mt-5 p-5">
       <div className="sk-chase mx-auto">
         <div className="sk-chase-dot"></div>
         <div className="sk-chase-dot"></div>
